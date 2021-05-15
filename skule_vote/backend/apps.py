6b7efb6bb337fc58975f78e20280fdaa76ae2fdc
Alex Bogdan <31147673+albogdan@users.noreply.github.com>
2021-05-15 16:02:46 -0400

    Created API and Backend apps (#19)
    * Created api and backend apps
    
    * Ran black on all apps

--- skule_vote/backend/apps.py
+++ skule_vote/backend/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FrontendConfig(AppConfig):
+class BackendConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "frontend"
+    name = "backend"

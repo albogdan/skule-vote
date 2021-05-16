33fa54a184108605ecf411c35b6862fe1dcdb34f
Armin <seyedarmin.alemohammad@mail.utoronto.ca>
2021-05-16 14:39:08 -0400

    Added unit test data for election sessions, elections, eligibility, and candidates. Added unit test to ensure fixtures load

--- skule_vote/backend/tests.py
+++ skule_vote/backend/tests.py
@@ -1,3 +1,9 @@
+from backend.models import ElectionSession
 from django.test import TestCase
 
 # Create your tests here.
+class FixturesTestCase(TestCase):
+    fixtures = ["test_data.json"]
+
+    def test_loaded_election_sessions(self):
+        self.assertEqual(ElectionSession.objects.count(), 2)

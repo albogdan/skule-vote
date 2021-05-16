f707e689ebbfc3bdcb7b9f8c2bb7c043a97ebb27
Lisa Li <lisasiaili@gmail.com>
2021-05-16 11:37:32 -0400

    Fixed broken tests

--- skule_vote/frontend/ui/src/components/__tests__/Header.test.js
+++ skule_vote/frontend/ui/src/components/__tests__/Header.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Header from "components/Header";
+import { withRouter } from "assets/testing";
 
 describe("<Header />", () => {
   it("renders header with nav bar", () => {
-    const { getByText } = render(<Header />);
+    const { getByText } = render(withRouter(<Header />));
     expect(getByText("Skule Vote")).toBeInTheDocument();
     expect(getByText("Vote")).toBeInTheDocument();
     expect(getByText("Check status")).toBeInTheDocument();

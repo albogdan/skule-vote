ebda61e687b4bbd39a35af9bddb4d345262e67fd
Alex Bogdan <31147673+albogdan@users.noreply.github.com>
2021-05-15 22:37:41 -0400

    Create Models (#25)
    * Added api and backend apps to django installed apps so they appear in the admin site
    
    * Created models and initial migration for app backend. Added the modules to the admin panel as well
    
    * Black formatting on migration
    
    * Set null=True and blank=True for Candidates to allow creation of Candidate with no preamble and/or blurb

--- skule_vote/backend/admin.py
+++ skule_vote/backend/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.models import (
+    ElectionSession,
+    Election,
+    Candidate,
+    Voter,
+    Ballot,
+    Eligibility,
+    Message,
+)
+
+
+@admin.register(ElectionSession)
+class ElectionSessionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Election)
+class ElectionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Voter)
+class VoterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ballot)
+class BallotAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Eligibility)
+class EligibilityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass

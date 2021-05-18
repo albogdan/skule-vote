d0f6f4a427a87f88604d0f761208b5dcb5527ffa
Lisa Li <lisasiaili@gmail.com>
2021-05-18 12:10:19 -0400

    modified tests

--- skule_vote/frontend/ui/src/assets/testing.js
+++ skule_vote/frontend/ui/src/assets/testing.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 export const withRouter = (component) => (
   <BrowserRouter>{component}</BrowserRouter>
 );
+
+export const withHistoryRouter = (component, route) => {
+  const history = createMemoryHistory();
+  history.push(route);
+  return <Router history={history}>{component}</Router>;
+};

bf2b1bfe933e18cd39058cf2faa3b495d5c33122
Armin <seyedarmin.alemohammad@mail.utoronto.ca>
2021-05-17 21:35:43 -0400

    Added serializers for candidate and election models

---
+++ skule_vote/backend/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from backend.models import Election, Candidate
+
+
+class CandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = (
+            "id",
+            "candidate_name",
+            "blurb",
+            "preamble",
+            "disqualified_status",
+            "disqualified_link",
+            "disqualified_blurb",
+            "rule_violation_message",
+            "rule_violation_link",
+        )
+
+class ElectionSerializer(serializers.ModelSerializer):
+    # This will query all candidates for a given election and nest their serialized representation in the
+    # "candidates" field as a list
+    candidates = CandidateSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Election
+        fields = (
+            "id",
+            "election_name",
+            "category",
+            "seats_available",
+            "candidates",
+        )

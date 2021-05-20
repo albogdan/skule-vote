99740961eed54e3611e04c373c1d2febda735cb8
Armin <seyedarmin.alemohammad@mail.utoronto.ca>
2021-05-20 12:58:22 -0400

    Added tests for various eligibility scenarios

--- skule_vote/backend/tests.py
+++ skule_vote/backend/tests.py
@@ -7,11 +7,17 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from skule_vote.tests import SetupElectionsMixin
-from backend.models import DISCIPLINE_CHOICES, STUDY_YEAR_CHOICES, Election, Eligibility
+from backend.models import (
+    DISCIPLINE_CHOICES,
+    STUDY_YEAR_CHOICES,
+    Election,
+    Eligibility,
+    Voter,
+)
 
 DISCIPLINES_POST_DICT = {
-    discipline_tuple[0]: f"AE{discipline_tuple[0]}BLAH"
-    for discipline_tuple in DISCIPLINE_CHOICES
+    discipline_code: f"AE{discipline_code}BLAH"
+    for discipline_code, discipline_name in DISCIPLINE_CHOICES
 }
 
 
@@ -61,18 +67,14 @@ def urlencode_cookie_request(
     }
 
 
-class GetElectionsTestCase(SetupElectionsMixin, APITestCase):
+class GetCookieTestCase(APITestCase):
     def setUp(self):
         super().setUp()
         self.elections_view = reverse("api:backend:election-list")
         self.cookie_view = reverse("api:backend:cookie")
 
-    def test_no_cookie(self):
-        response = self.client.get(self.elections_view)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
     def test_get_cookie(self):
-        voter_dict = urlencode_cookie_request()
+        voter_dict = urlencode_cookie_request(year=1, pey=False, discipline="ENG")
 
         # When sending requests with client.post, django moves url query params to request.GET in the view :(
         # That's why we need to send them as a dictionary in the data field so we can read them in POST
@@ -83,6 +85,68 @@ class GetElectionsTestCase(SetupElectionsMixin, APITestCase):
             self.client.cookies["student_number_hash"].value.split(":")[0],
             voter_dict["pid"],
         )
+
+        voter = Voter.objects.get(student_number_hash=voter_dict["pid"])
+        self.assertEqual(voter.student_status, "full_time")
+        self.assertEqual(voter.study_year, 1)
+        self.assertEqual(voter.pey, False)
+        self.assertEqual(voter.discipline, "ENG")
+        self.assertEqual(voter.engineering_student, True)
+
+    def test_nonregistered_student(self):
+        voter_dict = urlencode_cookie_request(registered=False)
+
+        response = self.client.post(self.cookie_view, voter_dict, follow=True)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # Make sure the view didn't create a voter entry
+        with self.assertRaises(Voter.DoesNotExist):
+            Voter.objects.get(student_number_hash=voter_dict["pid"])
+
+    def test_nonengineering_student(self):
+        voter_dict = urlencode_cookie_request(engineering=False)
+
+        response = self.client.post(self.cookie_view, voter_dict, follow=True)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # Make sure the view didn't create a voter entry
+        with self.assertRaises(Voter.DoesNotExist):
+            Voter.objects.get(student_number_hash=voter_dict["pid"])
+
+    def test_nonundergrad_student(self):
+        voter_dict = urlencode_cookie_request(undergrad=False)
+
+        response = self.client.post(self.cookie_view, voter_dict, follow=True)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # Make sure the view didn't create a voter entry
+        with self.assertRaises(Voter.DoesNotExist):
+            Voter.objects.get(student_number_hash=voter_dict["pid"])
+
+    def test_nonstudent(self):
+        voter_dict = urlencode_cookie_request(student=False)
+
+        response = self.client.post(self.cookie_view, voter_dict, follow=True)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # Make sure the view didn't create a voter entry
+        with self.assertRaises(Voter.DoesNotExist):
+            Voter.objects.get(student_number_hash=voter_dict["pid"])
+
+
+class GetElectionsTestCase(SetupElectionsMixin, APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.elections_view = reverse("api:backend:election-list")
+        self.cookie_view = reverse("api:backend:cookie")
+
+    def test_no_cookie(self):
+        response = self.client.get(self.elections_view)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_modify_cookie(self):
         # Make sure the election endpoint notices that we change the student number hash
@@ -109,26 +173,25 @@ class GetElectionsTestCase(SetupElectionsMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_class_reps(self):
-        for discipline_tuple in DISCIPLINE_CHOICES:
-            if discipline_tuple[0] == "ENG":
+        for discipline_code, discipline_name in DISCIPLINE_CHOICES:
+            if discipline_code == "ENG":
                 voter_dict = urlencode_cookie_request(year=1, discipline="ENG")
                 response = self.client.post(self.cookie_view, voter_dict, follow=True)
                 self.assertEqual(response.status_code, status.HTTP_200_OK)
 
                 response = self.client.get(self.elections_view)
-                print(response.json())
                 class_reps = [
                     elec
                     for elec in response.json()
                     if elec["category"] == "class_representative"
                 ]
                 self.assertEqual(len(class_reps), 1)
-                self.assertTrue(discipline_tuple[1] in class_reps[0]["election_name"])
+                self.assertTrue(discipline_name in class_reps[0]["election_name"])
                 continue
 
-            for year_tuple in STUDY_YEAR_CHOICES:
+            for year, year_name in STUDY_YEAR_CHOICES:
                 voter_dict = urlencode_cookie_request(
-                    year=year_tuple[0], discipline=discipline_tuple[0]
+                    year=year, discipline=discipline_code
                 )
                 response = self.client.post(self.cookie_view, voter_dict, follow=True)
                 self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -142,16 +205,16 @@ class GetElectionsTestCase(SetupElectionsMixin, APITestCase):
 
                 self.assertEqual(len(class_reps), 1)
                 self.assertTrue(
-                    discipline_tuple[1] in class_reps[0]["election_name"],
-                    (class_reps, year_tuple, discipline_tuple),
+                    discipline_name in class_reps[0]["election_name"],
+                    (class_reps, year, year_name, discipline_code, discipline_name),
                 )
                 self.assertTrue(
-                    year_tuple[1] in class_reps[0]["election_name"],
-                    (class_reps, year_tuple, discipline_tuple),
+                    year_name in class_reps[0]["election_name"],
+                    (class_reps, year, year_name, discipline_code, discipline_name),
                 )
 
             voter_dict = urlencode_cookie_request(
-                year=year_tuple[0], pey=True, discipline=discipline_tuple[0]
+                year=year, pey=True, discipline=discipline_code
             )
             response = self.client.post(self.cookie_view, voter_dict, follow=True)
             self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -164,11 +227,162 @@ class GetElectionsTestCase(SetupElectionsMixin, APITestCase):
             ]
 
             self.assertEqual(len(class_reps), 1)
-            self.assertTrue(
-                discipline_tuple[1] in class_reps[0]["election_name"],
-                (class_reps, "pey", discipline_tuple),
-            )
-            self.assertTrue(
-                "PEY" in class_reps[0]["election_name"],
-                (class_reps, "pey", discipline_tuple),
-            )
+            self.assertTrue(discipline_name in class_reps[0]["election_name"])
+            self.assertTrue("PEY" in class_reps[0]["election_name"])
+
+    def test_parttime_chair(self):
+        for attendance_code, expected in [("PT", True), ("FT", False)]:
+            for discipline_code, discipline_name in DISCIPLINE_CHOICES:
+                if discipline_code == "ENG":
+                    voter_dict = urlencode_cookie_request(
+                        year=1, discipline="ENG", attendance=attendance_code
+                    )
+                    response = self.client.post(
+                        self.cookie_view, voter_dict, follow=True
+                    )
+                    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                    response = self.client.get(self.elections_view)
+                    pt_chair = [
+                        elec
+                        for elec in response.json()
+                        if elec["category"] == "other"
+                        and elec["election_name"] == "Part-Time Chair"
+                    ]
+                    self.assertEqual(len(pt_chair), 1 if expected else 0)
+                    continue
+
+                for year, year_name in STUDY_YEAR_CHOICES:
+                    voter_dict = urlencode_cookie_request(
+                        year=year,
+                        discipline=discipline_code,
+                        attendance=attendance_code,
+                    )
+                    response = self.client.post(
+                        self.cookie_view, voter_dict, follow=True
+                    )
+                    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                    response = self.client.get(self.elections_view)
+                    pt_chair = [
+                        elec
+                        for elec in response.json()
+                        if elec["category"] == "other"
+                        and elec["election_name"] == "Part-Time Chair"
+                    ]
+                    self.assertEqual(len(pt_chair), 1 if expected else 0)
+
+    def test_officer(self):
+        for attendance_code in ["PT", "FT"]:
+            for discipline_code, discipline_name in DISCIPLINE_CHOICES:
+                if discipline_code == "ENG":
+                    voter_dict = urlencode_cookie_request(
+                        year=1, discipline="ENG", attendance=attendance_code
+                    )
+                    response = self.client.post(
+                        self.cookie_view, voter_dict, follow=True
+                    )
+                    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                    response = self.client.get(self.elections_view)
+                    pres = [
+                        elec
+                        for elec in response.json()
+                        if elec["category"] == "officer"
+                        and elec["election_name"] == "President"
+                    ]
+                    self.assertEqual(len(pres), 1)
+                    continue
+
+                for year, year_name in STUDY_YEAR_CHOICES:
+                    voter_dict = urlencode_cookie_request(
+                        year=year,
+                        discipline=discipline_code,
+                        attendance=attendance_code,
+                    )
+                    response = self.client.post(
+                        self.cookie_view, voter_dict, follow=True
+                    )
+                    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                    response = self.client.get(self.elections_view)
+                    pres = [
+                        elec
+                        for elec in response.json()
+                        if elec["category"] == "officer"
+                        and elec["election_name"] == "President"
+                    ]
+                    self.assertEqual(len(pres), 1)
+
+    def test_referendum(self):
+        for attendance_code in ["PT", "FT"]:
+            for discipline_code, discipline_name in DISCIPLINE_CHOICES:
+                if discipline_code == "ENG":
+                    voter_dict = urlencode_cookie_request(
+                        year=1, discipline="ENG", attendance=attendance_code
+                    )
+                    response = self.client.post(
+                        self.cookie_view, voter_dict, follow=True
+                    )
+                    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                    response = self.client.get(self.elections_view)
+                    referendum = [
+                        elec
+                        for elec in response.json()
+                        if elec["category"] == "referenda"
+                        and elec["election_name"] == "Random Club Levy"
+                    ]
+                    self.assertEqual(len(referendum), 1)
+                    continue
+
+                for year, year_name in STUDY_YEAR_CHOICES:
+                    voter_dict = urlencode_cookie_request(
+                        year=year,
+                        discipline=discipline_code,
+                        attendance=attendance_code,
+                    )
+                    response = self.client.post(
+                        self.cookie_view, voter_dict, follow=True
+                    )
+                    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                    response = self.client.get(self.elections_view)
+                    referendum = [
+                        elec
+                        for elec in response.json()
+                        if elec["category"] == "referenda"
+                        and elec["election_name"] == "Random Club Levy"
+                    ]
+                    self.assertEqual(len(referendum), 1)
+
+    def test_discipline_club_chair(self):
+        for attendance_code in ["PT", "FT"]:
+            for discipline_code, discipline_name in DISCIPLINE_CHOICES:
+                if discipline_code == "ENG":
+                    continue
+
+                for year, year_name in STUDY_YEAR_CHOICES:
+                    voter_dict = urlencode_cookie_request(
+                        year=year,
+                        discipline=discipline_code,
+                        attendance=attendance_code,
+                    )
+                    response = self.client.post(
+                        self.cookie_view, voter_dict, follow=True
+                    )
+                    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                    response = self.client.get(self.elections_view)
+                    election_name = (
+                        "ECE Club Chair"
+                        if discipline_code in ["CPE", "ELE"]
+                        else f"{discipline_name} Club Chair"
+                    )
+                    chair = [
+                        elec
+                        for elec in response.json()
+                        if elec["category"] == "discipline_club"
+                        and elec["election_name"] == election_name
+                    ]
+                    self.assertEqual(len(chair), 1)
--- skule_vote/skule_vote/tests.py
+++ skule_vote/skule_vote/tests.py
@@ -13,18 +13,28 @@ from backend.models import (
 
 class SetupElectionsMixin:
     def setUp(self):
-        curr_session = ElectionSession(
+        self.curr_session = ElectionSession(
             election_session_name="Summer 2021 Elections",
             start_time=datetime.now().replace(tzinfo=settings.TZ_INFO),
-            end_time=datetime(year=2022, month=12, day=23),
+            end_time=datetime(year=2022, month=12, day=23).replace(
+                tzinfo=settings.TZ_INFO
+            ),
         )
-        curr_session.save()
+        self.curr_session.save()
 
-        for discipline_tuple in DISCIPLINE_CHOICES:
-            if discipline_tuple[0] == "ENG":
+        self._create_referendum()
+        self._create_class_reps()
+        self._create_officer()
+        self._create_parttime_chair()
+        self._create_discipline_club_chair()
+
+    def _create_class_reps(self):
+        # Create class reps for all years, disciplines, and PEY
+        for discipline_code, discipline_name in DISCIPLINE_CHOICES:
+            if discipline_code == "ENG":
                 election = Election(
-                    election_name=f"{discipline_tuple[1]} Class Rep",
-                    election_session=curr_session,
+                    election_name=f"{discipline_name} Class Rep",
+                    election_session=self.curr_session,
                     seats_available=1,
                     category="class_representative",
                 )
@@ -32,7 +42,7 @@ class SetupElectionsMixin:
 
                 kwargs = {
                     "year_1_eligible": True,
-                    f"{discipline_tuple[0].lower()}_eligible": True,
+                    f"{discipline_code.lower()}_eligible": True,
                 }
                 elgibility = Eligibility(
                     election=election, status_eligible="full_and_part_time", **kwargs
@@ -40,18 +50,18 @@ class SetupElectionsMixin:
                 elgibility.save()
                 continue
 
-            for year_tuple in STUDY_YEAR_CHOICES:
+            for year, year_name in STUDY_YEAR_CHOICES:
                 election = Election(
-                    election_name=f"{year_tuple[1]} {discipline_tuple[1]} Class Rep",
-                    election_session=curr_session,
+                    election_name=f"{year_name} {discipline_name} Class Rep",
+                    election_session=self.curr_session,
                     seats_available=1,
                     category="class_representative",
                 )
                 election.save()
 
                 kwargs = {
-                    f"year_{year_tuple[0]}_eligible": True,
-                    f"{discipline_tuple[0].lower()}_eligible": True,
+                    f"year_{year}_eligible": True,
+                    f"{discipline_code.lower()}_eligible": True,
                 }
                 elgibility = Eligibility(
                     election=election, status_eligible="full_and_part_time", **kwargs
@@ -59,8 +69,8 @@ class SetupElectionsMixin:
                 elgibility.save()
 
             election = Election(
-                election_name=f"PEY {discipline_tuple[1]} Class Rep",
-                election_session=curr_session,
+                election_name=f"PEY {discipline_name} Class Rep",
+                election_session=self.curr_session,
                 seats_available=1,
                 category="class_representative",
             )
@@ -68,9 +78,120 @@ class SetupElectionsMixin:
 
             kwargs = {
                 f"pey_eligible": True,
-                f"{discipline_tuple[0].lower()}_eligible": True,
+                f"{discipline_code.lower()}_eligible": True,
             }
             elgibility = Eligibility(
                 election=election, status_eligible="full_and_part_time", **kwargs
             )
             elgibility.save()
+
+    def _create_parttime_chair(self):
+        election = Election(
+            election_name="Part-Time Chair",
+            election_session=self.curr_session,
+            seats_available=1,
+            category="other",
+        )
+        election.save()
+
+        kwargs = {
+            f"year_{year}_eligible": True for year, year_name in STUDY_YEAR_CHOICES
+        } | {
+            f"{discipline_code.lower()}_eligible": True
+            for discipline_code, discipline_name in DISCIPLINE_CHOICES
+        }
+        elgibility = Eligibility(
+            election=election, status_eligible="part_time", **kwargs
+        )
+        elgibility.save()
+
+    def _create_discipline_club_chair(self):
+
+        for discipline_code, discipline_name in DISCIPLINE_CHOICES:
+            # The goal is to test multiple years of the same discipline being eligible, so no point in testing track one
+            if discipline_code == "ENG":
+                continue
+
+            # Computer and electrical engineering share a club
+            if discipline_code == "CPE":
+                continue
+
+            if discipline_code == "ELE":
+                election = Election(
+                    election_name="ECE Club Chair",
+                    election_session=self.curr_session,
+                    seats_available=1,
+                    category="discipline_club",
+                )
+                election.save()
+
+                kwargs = {
+                    f"year_{year}_eligible": True
+                    for year, year_name in STUDY_YEAR_CHOICES
+                } | {"ele_eligible": True, "cpe_eligible": True}
+
+                elgibility = Eligibility(
+                    election=election, status_eligible="full_and_part_time", **kwargs
+                )
+                elgibility.save()
+                continue
+
+            # Other disciplines
+            election = Election(
+                election_name=f"{discipline_name} Club Chair",
+                election_session=self.curr_session,
+                seats_available=1,
+                category="discipline_club",
+            )
+            election.save()
+
+            kwargs = {
+                f"year_{year}_eligible": True for year, year_name in STUDY_YEAR_CHOICES
+            } | {f"{discipline_code.lower()}_eligible": True}
+
+            elgibility = Eligibility(
+                election=election, status_eligible="full_and_part_time", **kwargs
+            )
+            elgibility.save()
+
+    def _create_referendum(self):
+        election = Election(
+            election_name="Random Club Levy",
+            election_session=self.curr_session,
+            seats_available=1,
+            category="referenda",
+        )
+        election.save()
+
+        # everyone eligible
+        kwargs = {
+            f"year_{year}_eligible": True for year, year_name in STUDY_YEAR_CHOICES
+        } | {
+            f"{discipline_code.lower()}_eligible": True
+            for discipline_code, discipline_name in DISCIPLINE_CHOICES
+        }
+        elgibility = Eligibility(
+            election=election, status_eligible="full_and_part_time", **kwargs
+        )
+        elgibility.save()
+
+    def _create_officer(self):
+        election = Election(
+            election_name="President",
+            election_session=self.curr_session,
+            seats_available=1,
+            category="officer",
+        )
+        election.save()
+
+        # everyone eligible
+        kwargs = {
+            f"year_{year}_eligible": True for year, year_name in STUDY_YEAR_CHOICES
+        } | {
+            f"{discipline_code.lower()}_eligible": True
+            for discipline_code, discipline_name in DISCIPLINE_CHOICES
+        }
+        elgibility = Eligibility(
+            election=election, status_eligible="full_and_part_time", **kwargs
+        )
+        elgibility.save()

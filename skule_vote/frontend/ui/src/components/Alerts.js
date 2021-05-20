50912bb42f072354a3687857abbd424d0731d80c
Lisa Li <42653157+lisa-sa-li@users.noreply.github.com>
2021-05-20 11:57:20 -0600

    Create Alert component (#62)
    * add custom Alert component
    
    * added tests

---
+++ skule_vote/frontend/ui/src/components/Alerts.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import Alert from "@material-ui/lab/Alert";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { useTheme } from "@material-ui/core/styles";
+
+const fillColorMap = {
+  info: "#DDECF6",
+  warning: "#FDF0DF",
+  success: "#DFF3EB",
+  error: "#FBDFDF",
+};
+
+const AlertDiv = styled.div`
+  > div {
+    ${(props) => !props.isDark && `background-color: ${props.$fill}`};
+    width: 100%;
+    font-size: 16px;
+  }
+`;
+
+export const CustomAlert = ({ message, type, action }) => {
+  const theme = useTheme();
+  if (!["info", "warning", "success", "error"].includes(type)) {
+    return null;
+  }
+  const isDark = theme.palette.type === "dark";
+  const CloseBtn = (
+    <IconButton
+      aria-label="closeAlert"
+      color="inherit"
+      size="small"
+      onClick={action}
+    >
+      <CloseIcon data-testid="closeAlert" fontSize="inherit" />
+    </IconButton>
+  );
+
+  return (
+    <AlertDiv $fill={fillColorMap[type]} isDark={isDark}>
+      <Alert variant="outlined" severity={type} action={action && CloseBtn}>
+        {message}
+      </Alert>
+    </AlertDiv>
+  );
+};

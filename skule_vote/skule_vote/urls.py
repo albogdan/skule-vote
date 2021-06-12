9ee258f98a08bf845d116213fcdfeed9fdd1d7eb
AB <stefanalexb@gmail.com>
2021-06-11 22:38:08 -0400

    Fixed testing error by removing django-debug-toolbar

--- skule_vote/skule_vote/urls.py
+++ skule_vote/skule_vote/urls.py
@@ -45,7 +45,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

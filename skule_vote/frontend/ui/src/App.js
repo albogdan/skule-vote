0a704713ce1da4dacf598ba6017a93adca8a7191
Lisa Li <lisasiaili@gmail.com>
2021-07-20 22:45:03 -0600

    fixed some broken styles

--- skule_vote/frontend/ui/src/App.js
+++ skule_vote/frontend/ui/src/App.js
@@ -106,6 +106,12 @@ const App = () => {
                 fontSize: 16,
               },
             },
+            sizeLarge: {
+              fontSize: 20,
+              "@media (max-width:600px)": {
+                fontSize: 18,
+              },
+            },
           },
         },
         palette: {

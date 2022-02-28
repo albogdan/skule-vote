f3de311d371c97cbcca2a5fa46d42bd5e6336cdf
Lisa Li <lisasiaili@gmail.com>
2022-02-28 11:55:31 -0500

    fix broken unittests

--- skule_vote/frontend/ui/src/pages/__tests__/LandingPage.test.js
+++ skule_vote/frontend/ui/src/pages/__tests__/LandingPage.test.js
@@ -4,7 +4,31 @@ import { withRouter } from "assets/testing";
 import LandingPage from "../LandingPage";
 
 describe("<LandingPage />", () => {
-  it("renders landing page", () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules(); // Clears the cache
+    process.env = { ...OLD_ENV }; // Make a copy
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV; // Restore old environment
+  });
+
+  it("renders landing page on local", () => {
+    const { getByText, getByTestId } = render(withRouter(<LandingPage />));
+    expect(getByText("Welcome to SkuleVote")).toBeInTheDocument();
+    expect(getByText("Election Details")).toBeInTheDocument();
+    expect(getByText(/Single Transferable Vote system/i)).toBeInTheDocument();
+    expect(getByTestId("skuleLogo")).toBeInTheDocument();
+    expect(getByText("Vote").closest("a")).toHaveAttribute(
+      "href",
+      "/elections"
+    );
+  });
+
+  it("renders landing page on prod", () => {
+    process.env.REACT_APP_DEV_SERVER_URL = "not local";
     const { getByText, getByTestId } = render(withRouter(<LandingPage />));
     expect(getByText("Welcome to SkuleVote")).toBeInTheDocument();
     expect(getByText("Election Details")).toBeInTheDocument();
